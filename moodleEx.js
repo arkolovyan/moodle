ecaf67f14bc6eff41d34e6669fdc87c078b4701e
arkolovyan <arkolovyan@gmail.com>
2026-04-24 15:17:12 +1000

    Update moodleEx.js

--- moodleEx.js
+++ moodleEx.js
@@ -330,7 +330,6 @@ function applySignedInput(answerContainer) {
     input.insertAdjacentHTML('beforebegin', signed_input_html.replace('idSuffix', idSuffix));
     input.style.setProperty('display', 'none', 'important');
     let inp = content.querySelector('#signed_input_' + idSuffix);
-    inp.style.width = input.style.width;
     inp.value = input.value;
     if (input.getAttribute('readonly') || input.disabled) 
         inp.disabled = true;
