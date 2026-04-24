d8c95fc8f52c9233b4188dab4b45a9b525e46f02
arkolovyan <arkolovyan@gmail.com>
2026-04-24 12:29:28 +1000

    Update moodleEx.js

--- moodleEx.js
+++ moodleEx.js
@@ -338,7 +338,7 @@ function applySignedInput(answerContainer) {
         if (submitter.name == 'finish') {
             let v = parseFloat(inp.value.replace(",", "."));
             if (isNaN(v)) input.value = inp.value
-            else if (v > 0 && !inp.value.starsWith('+')) input.value = 'a' + inp.value;
+            else if (v > 0 && inp.value.indexOf('+') != 0) input.value = 99 + '' + inp.value;
             else input.value = inp.value;
         }
     });
