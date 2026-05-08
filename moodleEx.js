c49f36165be22d559cd2b5f9b3f328dd3c8c24f4
arkolovyan <arkolovyan@gmail.com>
2026-05-08 11:08:04 +1000

    Update moodleEx.js

--- moodleEx.js
+++ moodleEx.js
@@ -605,7 +605,7 @@ function applySignedInput(answerContainer, options) {
             val = parseFloat(inp_str);
         if (!isNaN(val)) {
             if (val > 999999) inp.value = formatFloat(inp_str.replace('9999999', ''))
-            else if (inp_str.startsWith('12345')) inp.value = inp_str.replace('12345', '')
+            else if (inp_str.startsWith('-9999999')) inp.value = inp_str.replace('-9999999', '')
             else if (val > 0 && inp_str.indexOf('+' == -1)) inp.value = '+' + formatFloat(val);
         }
     }
@@ -614,7 +614,7 @@ function applySignedInput(answerContainer, options) {
     form?.addEventListener('submit', function (event) {
         switch (event.submitter.name) {
             case 'finish':
-                if (missingPlus(inp.value)) input.value = '12345' + inp.value;
+                if (missingPlus(inp.value)) input.value = '-9999999' + inp.value;
                 break;
             case 'save':
                 if (missingPlus(inp.value)) input.value = '9999999​' + inp.value;
