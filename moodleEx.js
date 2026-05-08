862dc3ad31c917835704158d394a75214bc2e25c
arkolovyan <arkolovyan@gmail.com>
2026-05-08 11:38:43 +1000

    Update moodleEx.js

--- moodleEx.js
+++ moodleEx.js
@@ -604,7 +604,7 @@ function applySignedInput(answerContainer, options) {
         let inp_str = input.value.replace(',', '.'),
             val = parseFloat(inp_str);
         if (!isNaN(val)) {
-            if (val > 999999) inp.value = formatFloat(inp_str.replace('9999999', ''))
+            if (val > 999999) inp.value = formatFloat(inp_str.replace('999999', ''))
             else if (inp_str.startsWith('-9999999')) inp.value = inp_str.replace('-9999999', '')
             else if (val > 0 && inp_str.indexOf('+' == -1)) inp.value = '+' + formatFloat(val);
         }
